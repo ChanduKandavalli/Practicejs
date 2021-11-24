28b3df8a71d071de2cd1c4aa280554b1a9c1cd26
ChanduKandavalli <93129799+ChanduKandavalli@users.noreply.github.com>
2021-11-24 12:10:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.clear()
+var firstName="sadhu"
+console.log(this.firstName){
+},
+function Person(name,yob,job){
+  this.name=name
+  this.yob=yob
+  this.job
+}
+Person.prototype.getNameAndAge=function(){
+  return('name:'+this.name''+'age:'+(2021-this.year))
+}
+var srikanth=new Person('srikanth',1990,'business')
+console.log(srikanth.getNameAndAge())
